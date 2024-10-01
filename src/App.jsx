4d635c1ce301bb1cb5a18a2de9b9c45bd8964d14
EldaZAY <elda.aiyuz@gmail.com>
2024-09-30 19:26:58 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// import React from 'react'
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import HomeCards from "./components/HomeCards"
+import JobLisings from "./components/JobLisings"
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <HomeCards />
+
+    <JobLisings />
+
+    <section className="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+// import React from 'react'
+
+const Card = ({children, bg="bg-gray-100", rounded="rounded-lg", shadow="shadow-md"}) => {
+  return (
+    <div className={`${bg} ${rounded} ${shadow} p-6`}>
+        {children}
+    </div>
+  )
+}
+
+export default Card
