190a2bc798551237fa6cb9f13b58375e3c66c726
EldaZAY <elda.aiyuz@gmail.com>
2024-10-02 18:18:38 -0700

    layout, routes, Link

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 // import React from 'react'
 
-const Card = ({children, bg="bg-gray-100", rounded="rounded-lg", shadow="shadow-md"}) => {
-  return (
-    <div className={`${bg} ${rounded} ${shadow} p-6`}>
-        {children}
-    </div>
-  )
-}
+const Card = ({
+  children,
+  bg = "bg-gray-100",
+  rounded = "rounded-lg",
+  shadow = "shadow-md",
+}) => {
+  return <div className={`${bg} ${rounded} ${shadow} p-6`}>{children}</div>;
+};
 
-export default Card
+export default Card;
